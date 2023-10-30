594350923c6954c8453873c41dacd6bf5606d87d
DuvvuLakshmiPrasanna <147488264+DuvvuLakshmiPrasanna@users.noreply.github.com>
2023-10-31 00:45:39 +0530

    #include <stdio.h> #include <math.h>
    int main()
    {
    double side1, side2, hypotenuse;
    scanf("%lf", &side1);
    scanf("%lf", &side2);
    hypotenuse = sqrt(side1 * side1 + side2 * side2);
    printf("%.2lf
    ", hypotenuse);
    
        return 0;
    }

--- Hypotenuse.c
+++ Hypotenuse.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-int main() 
+
+int main()
 {
-    double side1, side2, hypotenuse;
-    scanf("%lf", &side1);
-    scanf("%lf", &side2);
-    hypotenuse = sqrt(side1 * side1 + side2 * side2);
-    printf("%.2lf
+double side1, side2, hypotenuse;
+scanf("%lf", &side1);
+scanf("%lf", &side2);
+hypotenuse = sqrt(side1 * side1 + side2 * side2);
+printf("%.2lf
 ", hypotenuse);
+
     return 0;
 }
-
