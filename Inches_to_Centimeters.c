c7ba597343dfb7eb2a9cd62d0e5b2b65cf4deae0
DuvvuLakshmiPrasanna <147488264+DuvvuLakshmiPrasanna@users.noreply.github.com>
2023-10-31 22:06:14 +0530

    #include <stdio.h> int main() {     double inches, centimeters;     scanf("%lf", &inches);     centimeters = inches * 2.54;     printf("%.2lf ",centimeters);
        return 0;
    }

---
+++ Inches_to_Centimeters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    double inches, centimeters;
+    scanf("%lf", &inches);
+    centimeters = inches * 2.54;
+    printf("%.2lf
+",centimeters);
+
+    return 0;
+}
