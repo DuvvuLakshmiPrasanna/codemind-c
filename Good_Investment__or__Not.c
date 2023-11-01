32ca74bd0e64688590cf9e649c6429533145fdfc
DuvvuLakshmiPrasanna <147488264+DuvvuLakshmiPrasanna@users.noreply.github.com>
2023-11-02 00:11:11 +0530

    #include<stdio.h> int main() {     int rate,a;     scanf("%d%d",&rate,&a);     if(rate>=a*2)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Good_Investment__or__Not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int rate,a;
+    scanf("%d%d",&rate,&a);
+    if(rate>=a*2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
